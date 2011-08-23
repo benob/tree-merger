127a9ff5476336464d29742d0eacb2bd904daa85
Benoit Favre <benoit.favre@gmail.com>
2011-08-23 22:33:51 +0200

    added license

--- view-hypergraph.py
+++ view-hypergraph.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python
 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
 import pygraphviz, re, sys, os
 
 tmp="/tmp/hypergraph"
